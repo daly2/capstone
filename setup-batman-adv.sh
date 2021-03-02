6f87b96ce1c321af482d87f2afa17646e3ec7919
Max Narvaez <mnmax.narvaez3@gmail.com>
2021-03-02 13:19:09 -0600

    Add sudo check and usb networking to script

--- setup-batman-adv.sh
+++ setup-batman-adv.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
-apt install -y batctl
+if [ $EUID -ne 0 ]
+then
+  echo "Please run as sudo"
+  exit 1
+fi
+
+mkdir /tmp/batctl-install
+
+(
+  cd /tmp/batctl-install || exit 1
+  wget https://downloads.open-mesh.org/batman/stable/sources/batctl/batctl-2020.4.tar.gz
+  tar -xf batctl-2020.4.tar.gz
+  cd batctl-2020.4 || exit 1
+  make
+  make install
+)
 
 if ! grep 'batman-adv' /etc/modules &> /dev/null
 then
   echo 'batman-adv' >> /etc/modules
 fi
 
-sed -i 's/^interface eth0/#interface eth0/' /etc/dhcpcd.conf
-sed -i 's/^metric 302/#metric 302/' /etc/dhcpcd.conf
-sed -i 's+^static ip-address=172.27.0.254/24+#static ip-address=172.27.0.254/24+' /etc/dhcpcd.conf
-sed -i 's/^static routers=172.27.0.1/#static routers=172.27.0.1/' /etc/dhcpcd.conf
-sed -i 's/^static domain_name_servers=172.27.0.1/#static domain_name_servers=172.27.0.1/' /etc/dhcpcd.conf
-sed -i 's/^nolink/#nolink/' /etc/dhcpcd.conf
-sed -i 's/interface wlan0/denyinterfaces wlan0/' /etc/dhcpcd.conf
+sed -i 's/interface eth0/interface usb0/' /etc/dhcpcd.conf
+sed -i 's/interface wlan0/denyinterfaces wlan0,bat0/' /etc/dhcpcd.conf
 sed -i 's/metric 202//' /etc/dhcpcd.conf
 
-cat << EOF > /usr/bin/start-batman-adv.sh
+cat << EOF > /usr/local/sbin/batman-start.sh
+#!/bin/bash
 sudo batctl if add wlan0
 
 sudo ifconfig wlan0 up
 sudo ifconfig bat0 up
 EOF
 
-chmod +x /usr/bin/start-batman-adv.sh
+chmod +x /usr/local/sbin/batman-start.sh
 
 cat << EOF > /lib/systemd/system/batman-start.service
 [Unit]
@@ -32,7 +43,7 @@ Before=multi-user.target
 
 [Service]
 Type=idle
-ExecStart=/usr/bin/start-batman-adv.sh
+ExecStart=/usr/local/sbin/batman-start.sh
 
 [Install]
 WantedBy=multi-user.target
